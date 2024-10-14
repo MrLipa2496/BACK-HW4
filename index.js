3098f6925ce5528b104b30786722ad535620ef1e
MrLipa2496 <sasalipa216@gmail.com>
2024-10-14 18:57:28 +0300

    Add requests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { sequelize, Phone } = require('./models');
 
 (async function () {
@@ -5,6 +6,7 @@ const { sequelize, Phone } = require('./models');
     await sequelize.sync({ force: true });
 
     //! CREATE
+    // додавання нового телефону,
     const phones = [
       {
         model: 'iPhone 13',
@@ -64,8 +66,93 @@ const { sequelize, Phone } = require('./models');
 
     for (const phone of phones) {
       const createdPhone = await Phone.create(phone);
-      console.log(`Phone created: ${JSON.stringify(createdPhone.get())}`);
+      //   console.log(createdPhone.get());
     }
+
+    // отримання списку телефонів (* 3-я сторінка при перегляді по 4 телефони на сторінці, упорядкованих за роком виробництва),
+    const page = 2;
+    const pageSize = 4;
+
+    const listOfPhones = await Phone.findAll({
+      raw: true,
+      order: [['yearOfManufacture', 'DESC']],
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+    });
+    // console.log(listOfPhones);
+
+    // отримання списку телефонів поточного року видання
+
+    const currentYear = new Date().getFullYear();
+
+    const phonesOfCurrentYear = await Phone.findAll({
+      raw: true,
+      where: {
+        yearOfManufacture: currentYear,
+      },
+    });
+
+    // console.log(phonesOfCurrentYear);
+
+    // отримання списку телефонів старше 2022 року випуску
+    const phonesOlder2022 = await Phone.findAll({
+      raw: true,
+      where: {
+        yearOfManufacture: {
+          [Op.lt]: 2022,
+        },
+      },
+    });
+
+    // console.log(phonesOlder2022);
+
+    //! UPDATE: змінити розмір оперативної пам'яті телефону з id: 1,
+    const updateRamSize = await Phone.update(
+      { ramSize: '16GB' },
+      {
+        raw: true,
+        returning: true,
+        where: {
+          id: 1,
+        },
+      }
+    );
+    // console.log(updateRamSize[1][0]);
+
+    //! UPDATE: додати нфс всім телефонам 2024 року випуску
+    const updateNFCFor2023Phones = await Phone.update(
+      { hasNFC: true },
+      { where: { yearOfManufacture: 2024 }, raw: true, returning: true }
+    );
+    // console.log(updateNFCFor2023Phones[1][0]);
+
+    // видалення телефону з id: 3.
+
+    const deletedPhone = await Phone.destroy({
+      where: { id: 3 },
+    });
+    // console.log(deletedPhone);
+
+    //! видалення телефонів 2015 року випуску
+    const deletedPhones2015 = await Phone.destroy({
+      where: { yearOfManufacture: 2015 },
+    });
+    // console.log(deletedPhones2015);
+
+    // вивести кількість телефонів кожної марки
+    const phoneCounts = await Phone.findAll({
+      attributes: [
+        'brand',
+        [sequelize.fn('COUNT', sequelize.col('id')), 'count'],
+      ],
+      group: ['brand'],
+      raw: true,
+    });
+
+    console.log('Кількість телефонів кожної марки:');
+    phoneCounts.forEach(phone => {
+      console.log(`Марка: ${phone.brand}, Кількість: ${phone.count}`);
+    });
   } catch (error) {
     console.error('Error creating phones:', error);
   }
