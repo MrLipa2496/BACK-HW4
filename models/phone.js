f02bb8545c733c939c700e162a88df25803ddefc
MrLipa2496 <sasalipa216@gmail.com>
2024-10-14 17:42:32 +0300

    Edit model description

--- models/phone.js
+++ models/phone.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Phone extends Model {
     /**
@@ -9,21 +7,58 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
-  Phone.init({
-    model: DataTypes.STRING,
-    brand: DataTypes.STRING,
-    yearOfManufacture: DataTypes.INTEGER,
-    ramSize: DataTypes.STRING,
-    processor: DataTypes.STRING,
-    screenSize: DataTypes.STRING,
-    hasNFC: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Phone',
-  });
+  Phone.init(
+    {
+      model: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [5, 50],
+        },
+      },
+
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [5, 50],
+        },
+      },
+
+      yearOfManufacture: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: {
+            args: 1900,
+            msg: 'Year of manufacture must be greater than 1900',
+          },
+          isBefore: new Date().toISOString(),
+        },
+      },
+
+      ramSize: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          isIn: {
+            args: [['2GB', '4GB', '8GB', '16GB', '32GB', '64GB']],
+            msg: 'RAM size must be one of the predefined options',
+          },
+        },
+      },
+      processor: DataTypes.STRING,
+      screenSize: DataTypes.STRING,
+      hasNFC: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'Phone',
+    }
+  );
   return Phone;
 };
